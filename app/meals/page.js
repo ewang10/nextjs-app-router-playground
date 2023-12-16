b2ad624c1c54cb3dc50364fe0149560817118512
eric <ericwang27759@gmail.com>
2023-12-15 19:46:15 -0800

    setting up DB, populate meals page, community page, and set up nav links

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,36 @@
 import Link from "next/link";
+import { Suspense } from "react";
+
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+const Meals = () => {
+    const meals = getMeals();
+
+    return <MealsGrid meals={meals} />;
+}
 
 const MealsPage = () => (
-    <main>
-        <h1>Meals</h1>
-        <p><Link href="/meals/share">Share</Link></p>
-        <p><Link href="/meals/meal-1">Meal 1</Link></p>
-    </main>
+    <>
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created{" "}
+                <span className={classes.highlight}>by you</span>
+            </h1>
+            <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">
+                    Share Your Favorite Recipe
+                </Link>
+            </p>
+        </header>
+        <main className={classes.main}>
+            <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                <Meals />
+            </Suspense>
+        </main>
+    </>
 );
 
 export default MealsPage;

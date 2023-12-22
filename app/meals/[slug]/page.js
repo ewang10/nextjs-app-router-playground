85add1633f1c702eb9a88a647b6669cde0e55019
eric <ericwang27759@gmail.com>
2023-12-21 22:46:28 -0800

    set up error and not found pages for general and meals, populate meals slug

--- app/meals/[slug]/page.js
+++
@@ -1,7 +0,0 @@
-const MealDetailsPage = () => (
-    <main>
-        <h1>Meal Details</h1>
-    </main>
-);
-
-export default MealDetailsPage;
---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+const Error = () => (
+    <main className="error">
+        <h1>An error occurred!</h1>
+        <p>Failed to fetch meal data. Please try again later.</p>
+    </main>
+);
+
+export default Error;

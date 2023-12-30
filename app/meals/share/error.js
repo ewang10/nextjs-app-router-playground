c3050f8564e0cee3d15be7c042d961495c92968a
eric <ericwang27759@gmail.com>
2023-12-29 22:52:48 -0800

    added form submission validation and  status

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+const Error = () => (
+    <main className="error">
+        <h1>An error occurred!</h1>
+        <p>Failed to create meal.</p>
+    </main>
+);
+
+export default Error;
